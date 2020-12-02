126731c516d31e7bf0491f3034ccecdb79d648fb
CatherineCMcGeoch <53623977+CatherineCMcGeoch@users.noreply.github.com>
2020-12-02 18:34:40 -0500

    Update min_vertex_color.py

--- dwave_networkx/examples/min_vertex_color.py
+++ dwave_networkx/examples/min_vertex_color.py
@@ -13,7 +13,7 @@
 #    limitations under the License.
 #
 # ================================================================================================
-from __future__ import print_function
+
 import dwave_networkx as dnx
 import networkx as nx
 import dimod
