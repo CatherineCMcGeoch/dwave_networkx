f5af7ca977770a98d6d7c4c91688cf271672df64
CatherineCMcGeoch <53623977+CatherineCMcGeoch@users.noreply.github.com>
2020-12-02 18:35:32 -0500

    Update min_vertex_color.py
    Deleted the line

--- dwave_networkx/examples/min_vertex_color.py
+++ dwave_networkx/examples/min_vertex_color.py
@@ -13,7 +13,7 @@
 #    limitations under the License.
 #
 # ================================================================================================
-
+  
 import dwave_networkx as dnx
 import networkx as nx
 import dimod
